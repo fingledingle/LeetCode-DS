c01ca4fc35f157a3392eb60d6c2410a86868f4ba
fingledingle <elenai@riseup.net>
2024-09-13 03:50:23 +0900

    hi

---
+++ test.js
@@ -0,0 +1,17 @@
+var isSubsequence = function(s, t) {
+    let first = 0;
+    let second = 0;
+
+    while(first < s.length && second < t.length){
+        if(s[first] == t[second]){
+            first ++;
+        }
+        second++
+    }
+
+    return first == s.length ? true : false;
+};
+
+
+
+console.log(isSubsequence('abc', 'ahbgdc'))
