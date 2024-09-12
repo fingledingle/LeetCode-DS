38f1175b126c0136417c06ac11fc30dfc58c1012
fingledingle <elenai@riseup.net>
2024-09-12 21:43:37 +0900

    third example using two arrays

--- Arrays and Strings/Two Pointers/ThirdExample.js
+++ Arrays and Strings/Two Pointers/ThirdExample.js
@@ -8,37 +8,36 @@ const secondArray = [3,5,6];
 
 
 function sortThem(arr1, arr2){
-    console.log(arr1)
-    console.log(arr2)
     const newArr = []; //making empty new array to return at the end and add them through out hte process
-    let left = 0;
-    let right = 0;
+    let left = 0; //starting the left for the first array
+    let left2 = 0; //starting the left for the second array
 
 
-    while (left < arr1.length && right < arr2.length){
-        if(arr1[left] < arr2[right]){
+    while (left < arr1.length && left2 < arr2.length){ //firsst while loop to sort them both without thinking if one array is bigger or not
+        if(arr1[left] < arr2[left2]){ //if the number at the certain index is smaller than the number on the second array at a certain index then add the first number to new list
             newArr.push(arr1[left]);
             left ++;
         }
 
-        if(arr2[right] < arr1[left]){
-            newArr.push(arr2[right]);
-            right++;
+        if(arr2[left2] < arr1[left]){ //if the number at the certain index is smaller than the number on the first array at a certain index then add the second number to new list
+            newArr.push(arr2[left2]);
+            left2++;
         }
     }
 
+    //then after the first while is done you gotta do these to clear out the rest of the list knowing only one of them will be run depending on which array is bigger
 
-    while(arr1[left]){
+    while(arr1[left]){ //loop through the rest of the first array to add all of it to the list
         newArr.push(arr1[left]);
         left++
     }
 
-    while(arr2[right]){
+    while(arr2[left2]){ //loop through the rest of the second array to add all of it to the list
         newArr.push(arr1[left]);
         left++
     }
 
-    return newArr
+    return newArr;
 }
 
 
