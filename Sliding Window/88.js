200cbc66ab68303158628477e83e138a365cf779
fingledingle <elenai@riseup.net>
2024-09-16 09:38:32 +0900

    hi

--- Sliding Window/88.js
+++ Sliding Window/88.js
@@ -52,17 +52,21 @@ console.log(Merge([1,2,3,0,0,0], [2,5,6]))
 
 //better approach
 var merge = function(nums1, m, nums2, n) {
-    let left1 = m - 1;
-    let left2 = n - 1;
-    let k = m + n - 1;
+    let left1 = m - 1; //that will be the last elemement of the array nums1
+    let left2 = n - 1; //that will be the last element of the array nums2
+    let k = m + n - 1; //that will be the lenght since the quantity of numbers in the firt array + the quantity of numbers in the second array will be = the new array length
 
     while(left2 >= 0) {
-        if(left1 >= 0 && nums1[left1] > nums2[left2]) {
-            nums1[k--] = nums1[left1--];
+        if(left1 >= 0 && nums1[left1] > nums2[left2]) { //if the number in the first array is bigger than the number in the second array then
+            nums1[k] = nums1[left1]; //the number at index (last index) will be equals the number at the index of left1
+            k--; //then obviously we drecrease the value of k so the while can keep going
+            left1-- //and we also decrease the value of left1
         }
 
-        else{
-            nums1[k--] = nums2[left2--];
+        else{ //if the number at that index is not bigger than we do this
+            nums1[k] = nums2[left2]; //the number at the last index will be equals the number at the second array index
+            k--; //then we decrease the value of k
+            left2--; //then we decreasse the valeu of left2;
         }
     }
 
