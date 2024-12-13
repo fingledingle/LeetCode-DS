44a5a5171e90c5e14c17413dbc7cce653f51682c
fingledingle <elenai@riseup.net>
2024-12-13 20:13:38 +0900

    finished 219

---
+++ crashcourse/LeetCode75/219.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let set = new Set();
+
+    for(let i = 0; i < nums.length; i ++){
+        if(i > k){
+            //deleting the first item
+            set.delete(nums[i - k - 1])
+        }
+
+        if(set.has(nums[i])){
+            return true;
+        }
+
+        set.add(nums[i]);
+    }
+
+
+    return false;
+
+
+    return false;
+};
